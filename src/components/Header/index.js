9141564f4f10a666b0e2d81c847ae10b0e9b3b52
logannas <annacarolinafr36@gmail.com>
2020-12-09 11:36:30 -0300

    header arrumada com material ui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import '../index.css'
+import {AppBar, Button, Toolbar, makeStyles} from "@material-ui/core";
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+        header:{
+            backgroundColor: "#06CDDB",
+            fontFamily: "Helvetica",
+            color: "#02383C",
+        },
+
+        menuButton:{
+            fontFamily: "Open Sans, sans-serif",
+            fontWeight: 700,
+            size: "18px",
+            marginLeft: "38px",
+        },
+
+        toolbar: {
+            display: "flex",
+            justifyContent: "space-between",
+        },
+}));
+
+const headersData=[
+    {
+        label: "Home",
+    },
+];
+
 
 function Header(props){
+    const {header, menuButton, toolbar} = useStyles();
+
+    const getMenuButton = () =>{
+        return headersData.map(({label})=>{
+            return(
+                <Button className={menuButton}><Link to={"/"} class="Link">{label}</Link></Button>
+            );
+        });
+    };
+
+    const displayDesktop = () =>{
+        return (
+            <Toolbar className={toolbar}>
+                {props.title}
+                {getMenuButton()}
+            </Toolbar>
+        )
+    }
     return(
         <header>
-            <h1>{props.title}</h1>
+           <AppBar className={header}>{displayDesktop()}</AppBar>
         </header>
     );
 }
